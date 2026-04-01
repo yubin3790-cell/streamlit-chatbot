aa565e2d077077af84b62986ae24b1b240a9ee25
김유빈 <yubin3790@gmail.com>
2026-04-01 18:22:08 +0900

    에코봇 입력창을 만들었음
    입력하고 대답하는 UI 완성

--- echobot.py
+++ echobot.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 st.title(":orange[:material/smart_toy:] 에코봇")
+
+
+if prompt := st.chat_input("에코봇에게 물어보기"):
+    
+    with st.chat_message("user"):
+        st.write(prompt)
+
+    with st.chat_message("ai"):
+        st.write(prompt)
