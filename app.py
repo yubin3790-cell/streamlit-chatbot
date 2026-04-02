c21005635c3725ec9fecdac08941b66d96e33e18
김유빈 <yubin3790@gmail.com>
2026-04-02 17:27:31 +0900

    제미나이 만들었음
    제미나이 연동함!!! 내가 질문하면 대답해줌 ㄹㅇ 제미나이임!

--- app.py
+++ app.py
@@ -1,3 +1,53 @@
 import streamlit as st
+from google import genai
 
-st.title(":orange[:material/smart_toy:] Title")
+import os
+from dotenv import load_dotenv
+
+st.title(":blue[:material/smart_toy:] 송승호")
+st.caption("하이ㅣㅣ")
+MODEL_NAME = "gemini-2.5-flash"
+
+@st.cache_resource
+def get_client():
+    load_dotenv()
+    api_key = os.getenv("GEMINI_API_KEY")
+
+    if not api_key:
+        st.error("🔑API 키가 설정되지 않았습니다.")
+        st.stop()
+
+    return genai.Client(api_key=api_key)
+    
+client = get_client()
+
+if "chat_session" not in st.session_state:
+    st.session_state.chat_session = client.chats.create(
+        model=MODEL_NAME
+    )
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+
+if prompt := st.chat_input("승호와 대화하기"):
+    
+    with st.chat_message("user"):
+        st.write(prompt)
+        st.session_state.messages.append({
+            "role": "user",
+            "content": prompt
+            })
+
+    with st.chat_message("ai"):
+        response = st.session_state.chat_session.send_message(prompt)
+
+        st.write(response.text)
+        st.session_state.messages.append({
+            "role": "ai",
+            "content": response.text
+            })
